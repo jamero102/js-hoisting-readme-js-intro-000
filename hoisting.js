286a86ecc710b09196ade5aca2aa200b75eca406
Jane Amero <janie.amero@gmail.com>
2018-10-08 21:35:56 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -9,22 +9,18 @@ var lyric = "maybe";
 
 
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
   var thisIsCrazy = function (){
-    console.log("hey!!!")
+    console.log("hey!!!")// fix the code in here:
+  thisIsCrazy();
   }
 }
 
 function sayMyName() {
-  // fix the code in here:
+  function sayMy() {
+    console.log(name);
+    var name = "Kristin";  // fix the code in here:
   var name = "Cricky";
 
   sayMy();
-
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
   }
 }
